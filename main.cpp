c4c48fab1e306d8a6b86b1fe201ff4b7b22079d4
Alexander Medvedev <s28572@pjwstk.edu.pl>
2023-05-11 13:02:17 +0200

    searching for password is inroduced

--- main.cpp
+++ main.cpp
@@ -1,69 +1,81 @@
-#include <iostream>
+#include <random>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
-#include <string>
-#include <fstream>
 #include <filesystem>
-#include <random>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
 
 using std::cout, std::cin;
 
-void addPassword(std::ofstream &file) {
+// TODO Introduce password complexity check
+void addPassword(std::string path) {
+
+    auto file = std::ofstream(path, std::ios::app);
+
     cout << "Password name: ";
 
     auto name = std::string();
     cin >> name;
     file << "\n" + name + " ";
 
-    cout << "Enter 1 to enter your own password, 2 to generate a random one: ";
-    auto answer = int();
-    cin >> answer;
+    for (auto answer = int(); answer != 1 && answer != 2;) {
+        cout << "Enter 1 to enter your own password, 2 to generate a random one: ";
+        cin >> answer;
 
-    auto password = std::string();
-    switch (answer) {
-        case 1:
-            cout << "Password: ";
-            cin >> password;
-            file << password + " ";
-            break;
-        case 2:
-            cout << "Enter password length: ";
-            auto length = int();
-            cin >> length;
+        auto password = std::string();
+        switch (answer) {
+            case 1:
+                cout << "Password: ";
+                cin >> password;
+                file << password + " ";
+                break;
+            case 2:
+            {
+                cout << "Enter password length: ";
+                auto length = int();
+                cin >> length;
 
-            cout << "Lowercase or uppercase letters? L for lower, U for upper, B for both: ";
-            auto letters = char();
-            cin >> letters;
+                cout << "Lowercase or uppercase letters? L for lower, U for upper, B for both: ";
+                auto letters = char();
+                cin >> letters;
 
-            cout << "Include special characters? Y for yes, N for no: ";
-            auto specialChars = char();
-            cin >> specialChars;
+                cout << "Include special characters? Y for yes, N for no: ";
+                auto specialChars = char();
+                cin >> specialChars;
 
-            std::random_device rd;
-            std::uniform_int_distribution<int>lowercase(97, 122);
-            std::uniform_int_distribution<int>uppercase(65, 90);
-            std::uniform_int_distribution<int>special(35, 38);
-            std::uniform_int_distribution<int>typeOfChar(1, 3);
+                std::random_device rd;
+                std::uniform_int_distribution<int> lowercase(97, 122);
+                std::uniform_int_distribution<int> uppercase(65, 90);
+                std::uniform_int_distribution<int> special(35, 38);
+                std::uniform_int_distribution<int> typeOfChar(1, 3);
 
-            for (int i = 0; i < length; ++i) {
-                switch (typeOfChar(rd)) {
-                    case 1:
-                        if(letters == 'L' || letters == 'B') {
-                            password += (char) lowercase(rd);
-                            break;
-                        }
-                    case 2:
-                        if(letters == 'U' || letters == 'B') {
-                            password += (char) uppercase(rd);
-                            break;
-                        }
-                    case 3:
-                        if (specialChars == 'Y') {
-                            password += (char) special(rd);
-                        }
+                for (int i = 0; i < length; ++i) {
+                    switch (typeOfChar(rd)) {
+                        case 1:
+                            if (letters == 'L' || letters == 'B') {
+                                password += (char) lowercase(rd);
+                                break;
+                            }
+                        case 2:
+                            if (letters == 'U' || letters == 'B') {
+                                password += (char) uppercase(rd);
+                                break;
+                            }
+                        case 3:
+                            if (specialChars == 'Y') {
+                                password += (char) special(rd);
+                            }
+                    }
                 }
+                file << password + " ";
+                break;
             }
-            file << password + " ";
+            default:
+                cout << "Wrong input!";
+                break;
+        }
     }
 
     cout << "Category: ";
@@ -73,24 +85,45 @@ void addPassword(std::ofstream &file) {
     file << category + "\n";
 }
 
+void searchPassword(std::string path) {
+
+    auto file = std::ifstream(path, std::ios::in);
+
+    cout << "Enter a keyword to search for: ";
+    auto searchFor = std::string();
+    cin >> searchFor;
+
+    bool found = false;
+    for (auto line = std::string(); std::getline(file, line); ) {
+        auto word = std::string();
+        auto stream = std::stringstream(line);
+        while (stream >> word) {
+            if (word.find(searchFor) != std::string::npos) {
+                cout << line;
+                found = true;
+            }
+        }
+    }
+    if(!found) cout << "Not found";
+}
+
 int main() {
     for (int i = 0; i < 256; ++i) {
         cout << i << " " << (char) i << std::endl;
     }
+
 //    cout << "Enter the name of the file: ";
 //    auto path = std::string();
-
+//
 //    cin >> path;
-
+//
 //    if(!std::filesystem::exists(path)) {
 //        cout << "Let's create a password for this new file: ";
 //        auto file = std::ofstream(path, std::ios::app);
 //
 //    }
 
-    auto file = std::ofstream("password.txt", std::ios::app);
-
-    addPassword(file);
+    searchPassword("password.txt");
 
     return 0;
 }
